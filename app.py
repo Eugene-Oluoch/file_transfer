c0e1e0cb740e0dc6192e817517efc225f2c5da0e
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-11-08 12:42:24 +0300

    Refactored code to functions

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
+from user import getting_data_from_user
+from directory import print_allowed_directories
 from transfer import handle_file_transfer
-from directory import *
 
 
-all_allowed_directories = gets_allowed_directories_in_home()
-print_allowed_directories(all_allowed_directories)
-
-from_directory = input("Where would you like to transfer from? ")
-to_directory = input("Where would you like to transfer to? ")
-file_extension = input("What is the file extension of the file you want transfer? ")
-
-
-paths = generate_paths(from_directory,to_directory)
-
-handle_file_transfer(paths, file_extension)
+print_allowed_directories()
+user_data = getting_data_from_user()
+handle_file_transfer(user_data)
 
 
