b153372a0ed51abc1030556a185958de15aa642c
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-11-08 12:06:14 +0300

    Refactored paths generation code

--- app.py
+++ app.py
@@ -1,22 +1,17 @@
 from transfer import handle_file_transfer
-from directory import gets_allowed_directories_in_home, get_home_path, print_allowed_directories
-import os,glob
+from directory import *
 
 
 all_allowed_directories = gets_allowed_directories_in_home()
 print_allowed_directories(all_allowed_directories)
 
-from_Directory = input("Where would you like to transfer from? ")
-to_Directory = input("Where would you like to transfer to? ")
+from_directory = input("Where would you like to transfer from? ")
+to_directory = input("Where would you like to transfer to? ")
 file_extension = input("What is the file extension of the file you want transfer? ")
 
-home_directory = get_home_path()
 
-from_path = home_directory+f"/{from_Directory}"
-to_path = home_directory+f"/{to_Directory}"
+paths = generate_paths(from_directory,to_directory)
 
-os.chdir(from_path)
-
-handle_file_transfer(from_path, to_path, file_extension)
+handle_file_transfer(paths, file_extension)
 
 
