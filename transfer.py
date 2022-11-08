b4e22fa6e680d0d7c442174674fe0f71b898ac9c
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-11-08 12:17:05 +0300

    Destructured paths to two variables

--- transfer.py
+++ transfer.py
@@ -2,9 +2,11 @@ import glob, shutil,os
 from directory import get_home_path
 def handle_file_transfer(paths,file_extension): 
     
-    os.chdir(paths["from_path"])
+    from_path, to_path = paths["from_path"],paths["to_path"]
+    
+    os.chdir(from_path)
     
     for file in glob.glob(f"*.{file_extension}"):
-        shutil.move(f'{paths["from_path"]}/{file}', paths["to_path"])
+        shutil.move(f'{from_path}/{file}', to_path)
         
     print("Files transfered successfully.")
