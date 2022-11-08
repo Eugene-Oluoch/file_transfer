1d540cbd4477370ac2dc16ffe7ecb28f1f1b2be0
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-11-08 12:06:50 +0300

    Refactores file path access

--- transfer.py
+++ transfer.py
@@ -1,9 +1,10 @@
 import glob, shutil,os
 from directory import get_home_path
-def handle_file_transfer(from_path,to_path,file_extension): 
-
+def handle_file_transfer(paths,file_extension): 
+    
+    os.chdir(paths["from_path"])
+    
     for file in glob.glob(f"*.{file_extension}"):
-        print(file)
-        shutil.move(f"{from_path}/{file}", to_path)
+        shutil.move(f'{paths["from_path"]}/{file}', paths["to_path"])
         
     print("Files transfered successfully.")
