def2faed0e0814db3052db4346955525c2860f7d
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-11-09 13:18:12 +0300

    Added a validation file

--- user.py
+++ user.py
@@ -1,20 +1,6 @@
 from directory import *
+from validation import *
 
-# = "The given directories are not part of the home directory"
-# "The given from directory isn't part of the home directory"
-# "The given to directory isn't part of the home directory"
-def validate_input_directories(from_directory,to_directory):
-    all_allowed_directories = gets_allowed_directories_in_home()
-
-    if from_directory not in all_allowed_directories and to_directory not in all_allowed_directories:
-        return "both"
-    elif from_directory not in all_allowed_directories:
-        return "from"
-    elif to_directory not in all_allowed_directories:
-        return "to"
-    else:
-        return None
-    
 
 def getting_data_from_user():
 
@@ -22,24 +8,8 @@ def getting_data_from_user():
     to_directory = input("Where would you like to transfer to? ")
     file_extentions = input("What is the file extension of the file you want transfer? ")
 
-    has_error = validate_input_directories(from_directory,to_directory)
-    continue_to_next_step = False
-    while not continue_to_next_step:
-        if has_error in ["both", "from", "to"]:
-            if has_error == "both":
-                from_directory = input("Re-enter from directory: ")
-                to_directory = input("Re-enter to directory: ")
-            elif has_error == "from":
-                from_directory = input("Re-enter from directory: ")
-            elif has_error == "to":
-                to_directory = input("Re-enter to directory: ")
-        else:
-            continue_to_next_step = True
-        has_error = validate_input_directories(from_directory,to_directory)
-        
-
-
-
-
-    return generate_user_data(from_directory,to_directory,file_extentions)
+    valid_user_data = validate_user_input(from_directory,to_directory)
+    
+    valid_from_directory, valid_to_directory = valid_user_data["from_directory"], valid_user_data["to_directory"]
+    return generate_user_data(valid_from_directory,valid_to_directory,file_extentions)
 
