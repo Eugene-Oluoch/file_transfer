de7ec9b77140ca4323d0cf70ea51cf775bbb15d9
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-11-09 15:15:41 +0300

    file extension

--- user.py
+++ user.py
@@ -20,9 +20,11 @@ def getting_data_from_user():
 
     from_directory = input("Where would you like to transfer from? ")
     to_directory = input("Where would you like to transfer to? ")
-    file_extentions = input("What is the file extension of the file you want transfer? ")
+    file_extension = input("What is the file extension of the file you want transfer? ")
 
     has_error = validate_input_directories(from_directory,to_directory)
+
+    
     continue_to_next_step = False
     while not continue_to_next_step:
         if has_error in ["both", "from", "to"]:
@@ -33,13 +35,21 @@ def getting_data_from_user():
                 from_directory = input("Re-enter from directory: ")
             elif has_error == "to":
                 to_directory = input("Re-enter to directory: ")
+
+        elif valid_file_extension == False:
+            file_extension = input("Invalid file extension. Please provide a valid file extension.  ")
+            valid_file_extension = validate_file_extension(file_extension)     
+                       
         else:
             continue_to_next_step = True
+        valid_file_extension = validate_file_extension(file_extension)    
         has_error = validate_input_directories(from_directory,to_directory)
         
 
+    return generate_user_data(from_directory,to_directory,file_extension)
 
-
-
-    return generate_user_data(from_directory,to_directory,file_extentions)
-
+def validate_file_extension(extension):
+    file_extensions = ["jpg", "jpeg", "png", "mp3", "mp4", "txt", "pdf", "docx"]
+    if extension not in file_extensions:
+        return False
+    return True    
